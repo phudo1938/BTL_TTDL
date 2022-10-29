1c18ef36c49d978fa8976f0ce525a2554dd7a811
Trần Huy Nam <92014754+HuyNam1112@users.noreply.github.com>
2022-10-29 21:48:40 +0700

    #2
    update pop up

--- maps/js/main.js
+++ maps/js/main.js
@@ -7,6 +7,6 @@ $(".md_close").on("click", function () {
 $("#submit").on("click", function () {
     $(".md_overlay, .pop_up").removeClass( "active" );
 });
-$(".form_pop_up ul li").on("click", function () {
+$(".form_pop_up table tbody tr").on("click", function () {
     $(".md_overlay, .pop_up").removeClass( "active" );
 });
